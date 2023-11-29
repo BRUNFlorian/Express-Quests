1ebaa1262f81df8e91fc20aaebedeef72c346977
Florian BRUN <flo33.brun@hotmail.fr>
2023-11-29 21:51:24 +0100

    test POST users.test.js + movies.test.js OK

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
+const crypto = require("node:crypto");
 
 const app = require("../src/app");
+const database = require("../database");
 
 describe("GET /api/users", () => {
   it("should return all users", async () => {
@@ -9,6 +11,45 @@ describe("GET /api/users", () => {
     expect(response.headers["content-type"]).toMatch(/json/);
 
     expect(response.status).toEqual(200);
+  });
+});
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase.firstname).toStrictEqual(newUser.firstname);
+  });
+
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
 
