6ddc185dae45be239b83cd3fa4905eb9d7a2c99e
Florian BRUN <flo33.brun@hotmail.fr>
2023-12-03 15:24:08 +0100

    Add request DELETE for /api/movies/:id + /api/users/:id

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 const userControllers = require("./controllers/userControllers");
 
@@ -18,5 +19,11 @@ app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUsersById);
 app.post("/api/users", userControllers.postUsers);
 app.put("/api/users/:id", userControllers.updateUsers);
+app.delete("/api/users/:id", userControllers.deleteUser);
+
+// const validateMovie = require("./middlewares/validateMovie");
+
+// app.post("/api/movies", validateMovie, movieControllers.postMovie);
+
 
 module.exports = app;
